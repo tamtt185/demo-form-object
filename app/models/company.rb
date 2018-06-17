6fe79efaa4f5b49b9a8cdaf59389a0c09eab4e8f
zanichi18 <thanhtam12t3@gmail.com>
2018-06-17 16:59:07 +0700

    nested-attributes

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,6 @@
 class Company < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :address, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
   has_one :company, inverse_of: :user, dependent: :destroy
 
+  accepts_nested_attributes_for :company
+
   delegate :name, :address, to: :company, prefix: true, allow_nil: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
+  validates :name, presence: true
+  validates :password, presence: true
 end
